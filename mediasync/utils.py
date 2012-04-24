f898ec84ba42d524913dd82cfbc086cf5ce0884c
Sævar Öfjörð Magnússon <saevar@gitargrip.is>
2012-04-24 14:44:23 +0000

    Resolve path

--- mediasync/utils.py
+++ mediasync/utils.py
@@ -1,12 +1,13 @@
 import datetime
 import os
+from mediasync.conf import msettings
 
 def get_file_mtime(url, path):
     """
     Takes a path to a file and returns a timestamp for the last modified 
     time of the file.
     """
-
+    path = os.path.join(msettings['STATIC_ROOT'], path)
     try:
         return datetime.datetime.fromtimestamp(os.path.getmtime(os.path.abspath(path))).strftime('%S%M%H%d%m%y')
     except OSError:
